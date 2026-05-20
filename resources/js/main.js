0cbe8c3fe440d75aff50e04bd5b8b3ccb2e7fa7f
VijayChunduri <34672103+VijayChunduri@users.noreply.github.com>
2026-05-21 07:41:50 +1000

    feat: fix for auto scrolling

--- resources/js/main.js
+++ resources/js/main.js
@@ -115,6 +115,10 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     Automatically opens matching accordion section.
   */
+ /* Remove ANY hash so page does NOT auto-scroll */
+  if (window.location.hash) {
+    history.replaceState(null, null, window.location.pathname + window.location.search);
+  }
   const hash = window.location.hash.replace("#", "");
 
   if (hash) {
